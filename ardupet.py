4628f59c320e25710f03d7f622df6b02a2b07fa2
Ranieri Althoff <ranisalt@gmail.com>
2014-10-18 16:37:42 -0300

    Rename light to lamp

--- ardupet.py
+++ ardupet.py
@@ -1,11 +1,11 @@
 """ArduPET is an office automation implementation. It is originally designed to
 be use at the Tutorial Education Program (PET in pt-BR, hence the name) to turn
-lights on and off and unlock the door, while also checking the statuses.
+lamps on and off and unlock the door, while also checking the statuses.
 
 Routes:
-GET /light -- lists all lamps statuses
-GET /light/<id> -- get the status of the lamp identified by <id>
-PUT /light/<id> -- update the status of the lamp identified by <id>
+GET /lamp -- lists all lamps statuses
+GET /lamp/<id> -- get the status of the lamp identified by <id>
+PUT /lamp/<id> -- update the status of the lamp identified by <id>
 GET /door -- get the status of the door
 PUT /door -- unlock the door (unlocking relocks after some seconds)
 """
@@ -19,7 +19,7 @@ def lamp_index():
 	"""Return a listing of all lamps statuses."""
 	return '', 501
 
-@app.route('/lamp/<int:light_id>', methods=['GET', 'PUT'])
+@app.route('/lamp/<int:lamp_id>', methods=['GET', 'PUT'])
 def lamp_status(id):
 	"""Read or update the status of a single lamp.
 
